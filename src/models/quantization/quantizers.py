2db8d9ead78db075f84e71617bd316638ab5a9be
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-03-21 00:45:07 +0100

    Allow dynamic quantization method selection

--- src/models/quantization/quantizers.py
+++ src/models/quantization/quantizers.py
@@ -1,7 +1,40 @@
 from typing import Optional
 import torch
 
-from src.models.quantization import Quantizer
+import torch
+
+class Quantizer:
+    """
+    General quantizer class for reference. It can be used as identity operator: Quantizer(None, None).
+
+    :param bits: The total number of bits used for quantization.
+    :param integer: The number of bits allocated for the integer part.
+    :param integer: The number of bits allocated for the integer part.
+
+    :return: A quantized tensor with values constrained to the specified bit range.
+    """
+        
+    def __init__(self, bits: int, integer: int, **kwargs):
+        self.bits = bits
+        self.integer = integer
+
+    @classmethod
+    def new(cls, method: str, **kwargs):
+        """Allow to dinamically instantiate the quantizer based on the method only."""
+
+        # Include here all accepted quantization methods
+        assert method.lower() in ["fixed-point"]
+
+        if method.lower() == "fixed-point":
+            return FixedPointQuantizer(**kwargs)
+        return cls(**kwargs)
+      
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        return x
+    
+    def __repr__(self):
+        return f"Quantizer(bits={self.bits}, integer={self.integer})"
+
 
 class FixedPointQuantizer(Quantizer):
     """

d07dac309a7bb54d1974da86b3c5985c76e5e3b9
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-03-22 10:23:13 +0100

    Make `QuantizedBatchNorm1d` inherit directly from `nn.BatchNorm1d`

--- src/models/quantization/activations.py
+++ src/models/quantization/activations.py
@@ -14,38 +14,40 @@ class QuantizedReLU(nn.ReLU):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.quantizer(super().forward(x))
 
-class QuantizedBatchNorm1d(nn.Module):
-    def __init__(
-            self,
-            num_features: int,
-            qweight: Optional[Quantizer] = None,
-            qbias: Optional[Quantizer] = None,
-            qmean: Optional[Quantizer] = None,
-            qvar: Optional[Quantizer] = None
-        ):
-        super().__init__()
-        self.bn = nn.BatchNorm1d(num_features)
 
-        # If no quantizer provided, identity quantizer is used
+class QuantizedBatchNorm1d(nn.BatchNorm1d):
+    def __init__(
+        self,
+        num_features: int,
+        qweight: Optional[Quantizer] = None,
+        qbias: Optional[Quantizer] = None,
+        qmean: Optional[Quantizer] = None,
+        qvar: Optional[Quantizer] = None
+    ):
+        super().__init__(num_features)  # Initialize nn.BatchNorm1d
+
+        # If no quantizer provided, use an identity quantizer
         self.qweight = qweight or Quantizer(None, None)
         self.qbias = qbias or Quantizer(None, None)
         self.qmean = qmean or Quantizer(None, None)
         self.qvar = qvar or Quantizer(None, None)
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         if self.training:
             # During training, use standard BatchNorm but quantize parameters
-            self.bn.weight.data = self.qweight(self.bn.weight.data)
-            self.bn.bias.data = self.qbias(self.bn.bias.data)
-            return self.bn(x)
+            self.weight.data = self.qweight(self.weight.data)
+            self.bias.data = self.qbias(self.bias.data)
+            return super().forward(x)
         else:
             # During inference, use quantized parameters and statistics
-            weight = self.qweight(self.bn.weight)
-            bias = self.qbias(self.bn.bias)
-            mean = self.qmean(self.bn.running_mean)
-            var = self.qvar(self.bn.running_var)
-            
+            weight = self.qweight(self.weight)
+            bias = self.qbias(self.bias)
+            mean = self.qmean(self.running_mean)
+            var = self.qvar(self.running_var)
+
             return F.batch_norm(
                 x, mean, var, weight, bias,
-                training=False, momentum=self.bn.momentum, eps=self.bn.eps
+                training=False,
+                momentum=self.momentum,
+                eps=self.eps
             )

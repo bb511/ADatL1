77d5d7a8e8414073e202fdaa649758553034489d
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-03-28 10:12:24 +0100

    Updated `QVAE`

--- src/models/qvae.py
+++ src/models/qvae.py
@@ -2,6 +2,7 @@ from typing import Optional, Tuple
 
 import torch
 from torch import nn, optim
+import torch.nn.functional as F
 
 from omegaconf import OmegaConf, DictConfig
 from pytorch_lightning import LightningModule
@@ -15,71 +16,69 @@ class QVAE(L1ADLightningModule):
         self,
         encoder: nn.Module,
         decoder: nn.Module,
-        loss: nn.Module,
-        optimizer: optim.Optimizer,
-        scheduler: Optional[DictConfig] = None,
+        **kwargs
     ):
-        super().__init__()
-        self.loss = loss
+        super().__init__(model=None, **kwargs)
         self.encoder, self.decoder = encoder, decoder
-        self.save_hyperparameters(ignore=["encoder", "decoder", "loss"])
+        self.save_hyperparameters(ignore=["model", "encoder", "decoder", "loss"])
         
     def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
         z_mean, z_log_var, z = self.encoder(x)
         reconstruction = self.decoder(z)
         return z_mean, z_log_var, z, reconstruction
     
-    def model_step(self, batch: torch.Tensor) -> torch.Tensor:
-        batch_flat = torch.flatten(batch, start_dim=1)
-        z_mean, z_log_var, z, reconstruction = self.forward(batch_flat)
+    def _extract_batch(self, batch: tuple) -> torch.Tensor:
+        # TODO: Remove after debugging:
+        batch = batch[0]
+        return torch.flatten(batch, start_dim=1).to(dtype=torch.float32)
+    
+    def model_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
+        x = self._extract_batch(batch)
+
+        z_mean, z_log_var, z, reconstruction = self.forward(x)
         total_loss, reco_loss, kl_loss = self.loss(
             reconstruction=reconstruction,
             z_mean=z_mean,
             z_log_var=z_log_var,
-            target=batch_flat
+            target=x
         )
+
+        # Compute anomaly score
+        anomaly_score = self.get_anomaly_score(x)
+        if self.logger != None:
+            self.logger.experiment.add_histogram('val/anomaly_score_distribution', anomaly_score, self.current_epoch)
 
         return {
             "loss": total_loss,
             "loss_reco": reco_loss,
-            "loss_kl": kl_loss
+            "loss_kl": kl_loss,
         }
-        
-    def training_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
-        output = self.model_step(batch)
+    
+    def get_anomaly_score(self, x: torch.Tensor):
+        """Calculate anomaly score based on reconstruction error"""
+        z_mean, _, _ = self.encoder(x)
+        reconstruction = self.decoder(z_mean)
+        return F.mse_loss(reconstruction, x, reduction='none').sum(dim=1)
+    
 
-        loss = output.get("loss")
-        self.log_dict({f"train/{key}": value for key, value in output.items()})
-        return loss
-        
-    def validation_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
-        output = self.model_step(batch)
 
-        loss = output.get("loss")
-        self.log_dict({f"val/{key}": value for key, value in output.items()})
-        return loss
+class FeatureQVAE(QVAE):
+    """A feature extractor is passed first through the data."""
 
-    def test_step(self, batch: torch.Tensor, batch_idx: int) -> torch.Tensor:
-        output = self.model_step(batch)
+    def __init__(
+        self,
+        features: nn.Module,
+        model = None,
+        **kwargs
+    ):
+        super().__init__(model=None, **kwargs)
+        self.features = features
+        self.save_hyperparameters(ignore=["model", "encoder", "decoder", "loss", "features"])
 
-        loss = output.get("loss")
-        self.log_dict({f"test/{key}": value for key, value in output.items()})
-        return loss
-        
-    def configure_optimizers(self):
-        optimizer = LightningOptimizer(self.hparams.optimizer(params=self.parameters()))
+    def _extract_batch(self, batch: tuple) -> torch.Tensor:
+        batch = super()._extract_batch(batch)
+        with torch.no_grad():
+            batch = self.features(batch)
 
-        if self.hparams.scheduler:
-            scheduler = self.hparams.scheduler.scheduler(optimizer=optimizer)
-
-            scheduler_dict = OmegaConf.to_container(self.hparams.scheduler, resolve=True) # convert to normal dict
-            scheduler_dict.update({
-                    "scheduler": scheduler,
-            })
-
-            return {
-                "optimizer": optimizer,
-                "lr_scheduler": scheduler_dict
-            }
-        
-        return {"optimizer": optimizer}
+        import ipdb; ipdb.set_trace()
+    

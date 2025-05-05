1c099a3d7d58a93e3b64c2d6f74a7fb0dfef15b9
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-05-05 13:29:38 +0200

    Updated scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,15 +3,18 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=4
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=30G
 #SBATCH --time=24:00:00
 
 # python3 src/train.py \
 srun python3 src/train.py \
     --multirun \
-    experiment=qvae \
+    experiment=vicreg \
     trainer=ddp \
+    trainer.devices=4 \
     trainer.max_epochs=100 \
+    data.batch_size=16384 \
+    data.num_workers=50 \
     # trainer.limit_train_batches=1.23e-5 \
     # trainer.limit_val_batches=9.8261e-5 \
     # trainer.limit_test_batches=1.46e-4 \

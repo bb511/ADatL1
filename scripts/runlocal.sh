4e9fb2e9d87fc96893e033cca7d5e9ba57fa8b29
Patrick Odagiu <podagiu@student.ethz.ch>
2025-05-09 16:54:05 +0200

    Getting read for sweep.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -2,10 +2,10 @@ python3 src/train.py \
     --multirun \
     experiment=qvae \
     trainer=gpu \
-    trainer.max_epochs=1 \
-    trainer.limit_train_batches=1.23e-5 \
-    trainer.limit_val_batches=9.8261e-5 \
-    trainer.limit_test_batches=1.46e-4 \
+    model.loss.alpha=0.1, 0.2, 0.3 \ # from 0.1 to 1
+    model.optimizer.lr=0.01, 0.001, 0.0001, 0.0001 \
+    seed=511, 6023 \
+    trainer.max_epochs=100 \
     data.batch_size=16384 \
-    data.num_workers=64 \
+    data.num_workers=32 \
     data.pin_memory=True \

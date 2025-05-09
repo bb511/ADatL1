ded3baf55b9ec933fb5c95a7ca26416db91285c9
Patrick Odagiu <podagiu@student.ethz.ch>
2025-05-09 15:13:17 +0200

    Misc changes in the config.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -1,10 +1,11 @@
 python3 src/train.py \
     --multirun \
     experiment=qvae \
-    trainer=cpu \
+    trainer=gpu \
     trainer.max_epochs=1 \
     trainer.limit_train_batches=1.23e-5 \
     trainer.limit_val_batches=9.8261e-5 \
     trainer.limit_test_batches=1.46e-4 \
     data.batch_size=16384 \
-    data.num_workers=50 \
+    data.num_workers=64 \
+    data.pin_memory=True \

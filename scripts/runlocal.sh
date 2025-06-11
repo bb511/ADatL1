aea8805bb678e926a31d9e6c68850e0417423cff
Patrick Odagiu <podagiu@student.ethz.ch>
2025-06-11 19:32:03 +0200

    Misc changes. Code used to scan the lr-alpha space with floating point AE.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -1,11 +1,12 @@
 python3 src/train.py \
     experiment=axov4 \
-    model.optimizer.lr=0.001 \
+    model.optimizer.lr=0.00001 \
+    model.loss.alpha=0.1 \
     trainer.max_epochs=100 \
     data.batch_size=16384 \
     data.num_workers=1 \
     data.pin_memory=True \
     data.specialized_loader=True \
     data.device=cpu \
-    # model.loss.alpha=1 \
+    trainer=gpu0 \
     # logger.mlflow.run_name= \
--- src/models/components/qvae.py
+++ src/models/components/qvae.py
@@ -82,7 +82,7 @@ class QuantizedEncoder(nn.Module):
         return z_mean, z_log_var, z
 
 
-class Decoder(nn.Module):
+class QuantizedDecoder(nn.Module):
     """Decoder for AD@L1."""
 
     def __init__(
@@ -173,3 +173,50 @@ class Encoder(nn.Module):
         z_mean, z_log_var = self.z_mean(x), self.z_log_var(x)
         z = self.sampling((z_mean, z_log_var))
         return z_mean, z_log_var, z
+
+class Decoder(nn.Module):
+    """Decoder for AD@L1."""
+
+    def __init__(
+        self,
+        nodes: List[int],
+        init_weight: Optional[Callable] = lambda _: None,
+        init_bias: Optional[Callable] = lambda _: None,
+        init_last_weight: Optional[Callable] = None,
+        init_last_bias: Optional[Callable] = None,
+        batchnorm: bool = False,
+    ) -> None:
+        super().__init__()
+
+        self.init_weight = init_weight
+        self.init_bias = init_bias
+
+        init_last_weight = init_last_weight if init_last_weight else lambda _: None
+        init_last_bias = init_last_bias if init_last_bias else lambda _: None
+
+        # Build the decoder as a MLP (no quantization) with batch normalization
+        net_list = []
+        for idx in range(len(nodes) - 2):
+            net_list.append(nn.Linear(nodes[idx], nodes[idx + 1]))
+            net_list.append(nn.ReLU())
+
+        # Remove last activation.
+        net_list.pop()
+
+        self.net = nn.Sequential(*net_list)
+        self.net.apply(self._init_weights_and_biases)
+
+        # Apply initialization to the weight of the last Linear() layer
+        last_linear_layer = self.net[-1]
+        if init_last_weight != None:
+            init_last_weight(last_linear_layer.weight)
+        if init_last_weight != None and last_linear_layer.bias != None:
+            init_last_bias(last_linear_layer.bias)
+
+    def _init_weights_and_biases(self, m):
+        if isinstance(m, nn.Linear):
+            self.init_weight(m.weight)
+            self.init_bias(m.bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.net(x)

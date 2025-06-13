f1124422c2549461fc9f619482456a2a61d7c2c7
Patrick Odagiu <podagiu@student.ethz.ch>
2025-06-13 14:49:38 +0200

    Moved the code for the floating-point AE to a different file. Cleaned up the configs a bit.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -3,9 +3,6 @@ python3 src/train.py \
     model.optimizer.lr=0.00001 \
     model.loss.alpha=0.1 \
     trainer.max_epochs=100 \
-    data.batch_size=16384 \
-    data.num_workers=1 \
-    data.pin_memory=True \
     data.specialized_loader=True \
     data.device=cpu \
     trainer=gpu0 \

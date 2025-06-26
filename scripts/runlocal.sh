b148c3fcb175ad40533df2c91e76f8807e558d8d
Patrick Odagiu <podagiu@student.ethz.ch>
2025-06-26 14:24:24 +0200

    Added retry package to poetry. Cleaned up the configuration a bit.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -1,7 +1,7 @@
 python3 src/train.py \
+    -m \
     experiment=axov4 \
-    model.optimizer.lr=0.0001 \
     trainer.max_epochs=100 \
-    data.use_entire_val_dataset=True \
-    model.loss.alpha=0.1 \
+    model.optimizer.lr=0.00025 \
+    model.loss.alpha=0.9 \
     trainer=gpu0 \

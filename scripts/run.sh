68aef8ce43ad62ec8be074e2e9f99bcb50516299
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-06-29 23:43:14 +0200

    Updated CAP callback implementation

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,13 +9,13 @@
 # python3 src/train.py \
 srun python3 src/train.py \
     --multirun \
-    experiment=qvae \
-    trainer=gpu \
-    model.loss.alpha=1.0 \
+    experiment=axov4 \
+    trainer=gpu0 \
+    +callbacks@callbacks.approximation_capacity=cap \
+    model.loss.alpha=0.5 \
     trainer.max_epochs=10 \
     +trainer.num_sanity_val_steps=0 \
-    data.num_workers=0 \
-    data.batch_size=65536 \
+    # data.loader.num_workers=1 \
     # trainer.limit_train_batches=1.23e-4 \
     # trainer.limit_val_batches=9.8261e-4 \
     # trainer.limit_test_batches=1.46e-3 \

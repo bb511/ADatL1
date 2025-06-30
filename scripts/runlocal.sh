0b634f8aa4f61d2c4a83c07459906e6924cd2f3c
Patrick <podagiu@ethz.ch>
2025-06-30 11:34:39 +0200

    Adding visualisations for the resutls. WIP

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -2,6 +2,6 @@ python3 src/train.py \
     -m \
     experiment=axov4 \
     trainer.max_epochs=100 \
-    model.optimizer.lr=0.00025 \
-    model.loss.alpha=0.9 \
+    model.optimizer.lr=0.001 \
+    model.loss.alpha=0.5 \
     trainer=gpu0 \

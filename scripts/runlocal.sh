581cb0c2bf47275d93f1a6863ebdcac02c03c93a
Patrick Odagiu <podagiu@student.ethz.ch>
2025-07-06 22:48:40 +0200

    Just changed config stuff.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -1,8 +1,8 @@
 python3 src/train.py \
     -m \
     experiment=axov4 \
-    trainer.max_epochs=3 \
-    model.optimizer.lr=0.001 \
-    model.loss.alpha=0.6 \
-    trainer=gpu0 \
+    trainer.max_epochs=100 \
+    model.optimizer.lr=0.0001 \
+    model.loss.alpha=0.5 \
+    trainer=gpu3 \
     data.val_batches=1 \

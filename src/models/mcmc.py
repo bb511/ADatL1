e3f53f52fc1af7ed6e6d1de2962310692e5d5d33
Victor Jimenez Rodriguez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2025-12-07 19:43:35 +0100

    Mismatching device

--- src/models/mcmc.py
+++ src/models/mcmc.py
@@ -144,6 +144,8 @@ class LangevinSampler(nn.Module):
 
         :return: A dictionary containing the final sample and diagnostic information.
         """
+        device = next(model.parameters()).device
+        x = x.to(device)
 
         # Ensure gradient tracking
         x = x.clone().detach().requires_grad_(True)

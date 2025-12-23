b9ce37f0dc6c8044b216bb89e4e75f569a623fc2
Patrick <podagiu@ethz.ch>
2025-12-23 17:49:40 +0100

    Cleaned up the experiment scripts.

--- scripts/runlocal.sh
+++ scripts/runlocal.sh
@@ -1,20 +1,21 @@
-# taskset -c 0-1 \
-# python3 src/train.py \
-#     -m \
-#     hparams_search=vicreg_optuna \
-#     experiment=debug \
-#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-#     data.val_batches=4
-
-taskset -c 37-39 \
+# Short debug script.
+taskset -c 0-9 \
 python3 src/train.py \
     -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    hparams_search=vicreg_optuna \
-    experiment=vicreg \
+    experiment=debug \
     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    data.val_batches=4 \
-    trainer.devices=[2]
-    # trainer=cpu
+    # hparams_search=vicreg_optuna \
+
+
+# vicreg hyperparameter search on gpu.
+# taskset -c 37-39 \
+# python3 src/train.py \
+#     -m \
+#     hydra/launcher=submitit_local \
+#     hydra.launcher.cpus_per_task=1 \
+#     hydra.launcher.gpus_per_node=4 \
+#     hparams_search=vicreg_optuna \
+#     experiment=vicreg \
+#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+#     data.val_batches=4 \
+#     trainer.devices=[2]

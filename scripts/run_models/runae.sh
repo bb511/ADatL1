cd1c9d53f0ce3e90557a8b41bc08171e624cb56a
Patrick <podagiu@ethz.ch>
2026-03-09 23:13:23 +0200

    CAP fix config.

--- scripts/run_models/runae.sh
+++ scripts/run_models/runae.sh
@@ -95,6 +95,9 @@
 taskset -c 18-20 \
 python3 src/train.py \
     -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
     experiment=ae_agnostic \
     experiment_name=ae_agnostic_cap_vs_mse_search \
@@ -104,15 +107,12 @@ python3 src/train.py \
     evaluator_callbacks.reco=null \
     logger=none \
     hparams_search=ae_agnostic_optuna \
-    hydra.sweeper.study_name=test_capSigmoidMargin_vs_mse_b16k \
+    hydra.sweeper.study_name=capSigmoidMargin_vs_mse_b16k \
     hydra.sweeper.n_trials=100 \
     hydra.sweeper.sampler.n_startup_trials=40 \
     trainer=gpu \
-    trainer.max_epochs=1 \
+    trainer.max_epochs=100 \
     trainer.devices=[0]
-    # hydra/launcher=submitit_local \
-    # hydra.launcher.cpus_per_task=1 \
-    # hydra.launcher.gpus_per_node=4 \
 
 
 # AE agnostic MSE and threshold stability.

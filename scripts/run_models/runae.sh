31f7b5f10b83200a8aacf9e0970ad30d5edc9759
Patrick <podagiu@ethz.ch>
2026-03-19 15:01:21 +0100

    Prepared drift search.

--- scripts/run_models/runae.sh
+++ scripts/run_models/runae.sh
@@ -108,30 +108,6 @@
 
 
 # AE agnostic hyperparameter search - CAP vs MSE.
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=ae_agnostic \
-    experiment_name=ae_agnostic_cap_vs_mse_search \
-    callbacks.max_rate_mse_ckpt=null \
-    callbacks.cvar25_ema_ckpt=null \
-    evaluator.ckpts.summary=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=ae_agnostic_optuna \
-    hydra.sweeper.study_name=cap_vs_mse_b16k \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50\
-    trainer.devices=[0]
-
-
-# AE agnostic MSE and threshold stability.
 # taskset -c 0-2 \
 # python3 src/train.py \
 #     -m \
@@ -140,31 +116,55 @@ python3 src/train.py \
 #     hydra.launcher.gpus_per_node=4 \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=ae_agnostic \
-#     experiment_name=ae_agnostic_mse_vs_thres_search \
+#     experiment_name=ae_agnostic_cap_vs_mse_search \
 #     callbacks.max_rate_mse_ckpt=null \
 #     callbacks.cvar25_ema_ckpt=null \
-#     evaluator.ckpts.last=false \
 #     evaluator.ckpts.summary=null \
-#     evaluator_callbacks.cap_sn_zb=null \
 #     evaluator_callbacks.reco=null \
 #     logger=none \
 #     hparams_search=ae_agnostic_optuna \
-#     optimized_metric_config.main_metric.callback.name=mse_mean_top_vals \
-#     +optimized_metric_config.main_metric.callback.params.ckpt_name=main_val \
-#     +optimized_metric_config.main_metric.callback.params.test_ds=main_test \
-#     optimized_metric_config.main_metric.direction=minimize \
-#     optimized_metric_config.sec_metric.callback.name=thres_transfer \
-#     optimized_metric_config.sec_metric.callback.params.ckpt_name=null \
-#     +optimized_metric_config.sec_metric.callback.params.target_rate=0.25 \
-#     ~optimized_metric_config.sec_metric.callback.params.test_ds \
-#     optimized_metric_config.sec_metric.direction=minimize \
-#     hydra.sweeper.study_name=mse_vs_drift_b16k \
-#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.study_name=cap_vs_mse_b16k \
 #     hydra.sweeper.n_trials=100 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
-#     trainer.max_epochs=50 \
+#     trainer.max_epochs=50\
 #     trainer.devices=[0]
+
+
+# AE agnostic MSE and threshold stability.
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=ae_agnostic \
+    experiment_name=ae_agnostic_mse_vs_thres_search \
+    callbacks.max_rate_mse_ckpt=null \
+    callbacks.cvar25_ema_ckpt=null \
+    evaluator.ckpts.last=true \
+    evaluator.ckpts.summary=null \
+    evaluator_callbacks.cap_sn_zb=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=ae_agnostic_optuna \
+    optimized_metric_config.main_metric.callback.name=mse_mean_top_vals \
+    +optimized_metric_config.main_metric.callback.params.ckpt_name=last \
+    +optimized_metric_config.main_metric.callback.params.test_ds=zerobias \
+    optimized_metric_config.main_metric.direction=minimize \
+    optimized_metric_config.sec_metric.callback.name=thres_transfer \
+    optimized_metric_config.sec_metric.callback.params.ckpt_name=null \
+    +optimized_metric_config.sec_metric.callback.params.target_rate=0.25 \
+    ~optimized_metric_config.sec_metric.callback.params.test_ds \
+    optimized_metric_config.sec_metric.direction=minimize \
+    hydra.sweeper.study_name=mse_vs_drift_b16k \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # AE agnostic MSE and wasserstein distance search.

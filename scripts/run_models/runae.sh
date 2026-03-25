4aef9afd3c3c61fcf787a53f85ed8178dd3d8cca
Patrick <podagiu@ethz.ch>
2026-03-25 09:48:23 +0100

    CAP run q99 mse delta

--- scripts/run_models/runae.sh
+++ scripts/run_models/runae.sh
@@ -111,29 +111,29 @@
 
 
 # AE hyperparameter search semi-supervised mse q99.
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=ae \
-    experiment_name=ae_cvar_vs_mseq99_search \
-    callbacks.max_rate_mse_ckpt=null \
-    callbacks.stable_mse_ckpt=null \
-    evaluator.ckpts.last=false \
-    evaluator.ckpts.single=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=ae_optuna \
-    optimized_metric_config.sec_metric.callback.name=mse_q99 \
-    hydra.sweeper.study_name=cvar25eff_vs_mseq99_b16k \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
+# taskset -c 0-2 \
+# python3 src/train.py \
+#     -m \
+#     hydra/launcher=submitit_local \
+#     hydra.launcher.cpus_per_task=1 \
+#     hydra.launcher.gpus_per_node=4 \
+#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+#     experiment=ae \
+#     experiment_name=ae_cvar_vs_mseq99_search \
+#     callbacks.max_rate_mse_ckpt=null \
+#     callbacks.stable_mse_ckpt=null \
+#     evaluator.ckpts.last=false \
+#     evaluator.ckpts.single=null \
+#     evaluator_callbacks.reco=null \
+#     logger=none \
+#     hparams_search=ae_optuna \
+#     optimized_metric_config.sec_metric.callback.name=mse_q99 \
+#     hydra.sweeper.study_name=cvar25eff_vs_mseq99_b16k \
+#     hydra.sweeper.n_trials=100 \
+#     hydra.sweeper.sampler.n_startup_trials=150 \
+#     trainer=gpu \
+#     trainer.max_epochs=50 \
+#     trainer.devices=[0]
 
 
 # AE hyperparameter search semi-supervised cvar 10%.
@@ -190,28 +190,28 @@ python3 src/train.py \
 
 
 # AE agnostic hyperparameter search - CAP vs MSE q99.
-# taskset -c 64-66 \
-# python3 src/train.py \
-#     -m \
-#     hydra/launcher=submitit_local \
-#     hydra.launcher.cpus_per_task=1 \
-#     hydra.launcher.gpus_per_node=4 \
-#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-#     experiment=ae_agnostic \
-#     experiment_name=ae_agnostic_cap_vs_mseq99_search \
-#     callbacks.max_rate_mse_ckpt=null \
-#     callbacks.cvar25_ema_ckpt=null \
-#     evaluator.ckpts.summary=null \
-#     evaluator_callbacks.reco=null \
-#     logger=none \
-#     hparams_search=ae_agnostic_optuna \
-#     optimized_metric_config.sec_metric.callback.name=mse_q99 \
-#     hydra.sweeper.study_name=cap_vs_mseq99_b16k \
-#     hydra.sweeper.n_trials=100 \
-#     hydra.sweeper.sampler.n_startup_trials=150 \
-#     trainer=gpu \
-#     trainer.max_epochs=50 \
-#     trainer.devices=[0]
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=ae_agnostic \
+    experiment_name=ae_agnostic_cap_vs_mseq99_search \
+    callbacks.max_rate_mse_ckpt=null \
+    callbacks.cvar25_ema_ckpt=null \
+    evaluator.ckpts.summary=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=ae_agnostic_optuna \
+    optimized_metric_config.sec_metric.callback.name=mse_q99 \
+    hydra.sweeper.study_name=cap_vs_mseq99_b16k \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # Stability Agnostic Searches

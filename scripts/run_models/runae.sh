551e0789dfb71f77f9ccf96d86f9c783f40dbea2
Patrick <podagiu@ethz.ch>
2026-03-31 00:14:50 +0200

    CVaR25 q99 AE search better checkpointing.

--- scripts/run_models/runae.sh
+++ scripts/run_models/runae.sh
@@ -103,31 +103,6 @@
 # ------------------------
 
 # AE hyperparameter search semi-supervised.
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=ae \
-    experiment_name=ae_cvar_vs_mse_search \
-    callbacks.max_rate_mse_ckpt=null \
-    callbacks.stable_mse_q99_ckpt=null \
-    evaluator_callbacks.mse_loss_q99=null \
-    evaluator_callbacks.thres_transfer=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=ae_optuna \
-    hydra.sweeper.study_name=cvar25eff_vs_mse_b16k \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-
-# AE hyperparameter search semi-supervised mse q99.
 # taskset -c 0-2 \
 # python3 src/train.py \
 #     -m \
@@ -136,21 +111,46 @@ python3 src/train.py \
 #     hydra.launcher.gpus_per_node=4 \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=ae \
-#     experiment_name=ae_cvar_vs_mseq99_search \
+#     experiment_name=ae_cvar_vs_mse_search \
 #     callbacks.max_rate_mse_ckpt=null \
-#     callbacks.stable_mse_ckpt=null \
-#     evaluator.ckpts.last=false \
-#     evaluator.ckpts.single=null \
+#     callbacks.stable_mse_q99_ckpt=null \
+#     evaluator_callbacks.mse_loss_q99=null \
+#     evaluator_callbacks.thres_transfer=null \
 #     evaluator_callbacks.reco=null \
 #     logger=none \
 #     hparams_search=ae_optuna \
-#     optimized_metric_config.sec_metric.callback.name=mse_q99 \
-#     hydra.sweeper.study_name=cvar25eff_vs_mseq99_b16k \
+#     hydra.sweeper.study_name=cvar25eff_vs_mse_b16k \
 #     hydra.sweeper.n_trials=100 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+
+# AE hyperparameter search semi-supervised mse q99.
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=ae \
+    experiment_name=ae_cvar_vs_mseq99_search \
+    callbacks.max_rate_mse_ckpt=null \
+    callbacks.stable_mse_mean_top_ckpt=null \
+    evaluator_callbacks.mse_operational_mean=null \
+    evaluator_callbacks.thres_transfer=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=ae_optuna \
+    optimized_metric_config.sec_metric.callback.name=mse_q99 \
+    hydra.sweeper.study_name=cvar25eff_vs_mseq99_b16k \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # AE hyperparameter search semi-supervised cvar 10%.

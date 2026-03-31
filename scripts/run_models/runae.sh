bfe803c85a6513cfbf9cbb8b58566d0672203bb3
Patrick <podagiu@ethz.ch>
2026-03-31 22:29:10 +0200

    Readied AE stability search q99.

--- scripts/run_models/runae.sh
+++ scripts/run_models/runae.sh
@@ -243,41 +243,6 @@
 # ---------------------------
 
 # AE agnostic MSE and threshold stability.
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=ae_agnostic \
-    experiment_name=ae_agnostic_drift_vs_mse_search \
-    callbacks.anomaly_eff=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.stable_mse_q99_ckpt=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    ~evaluator.ckpts.single.loss_mse_q99 \
-    ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-    evaluator_callbacks.anomaly_efficiency=null \
-    evaluator_callbacks.mse_loss_q99=null \
-    evaluator_callbacks.cap_sn_zb=null \
-    evaluator_callbacks.wasserstein=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=ae_agnostic_optuna \
-    optimized_metric_config.main_metric.callback.name=thres_drift \
-    +optimized_metric_config.main_metric.callback.params.target_rate=0.25 \
-    optimized_metric_config.main_metric.direction=minimize \
-    hydra.sweeper.study_name=drift_vs_mse_b16k \
-    hydra.sweeper.direction='[minimize, minimize]' \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-
-# AE agnostic MSE and threshold stability q99.
 # taskset -c 0-2 \
 # python3 src/train.py \
 #     -m \
@@ -286,15 +251,15 @@ python3 src/train.py \
 #     hydra.launcher.gpus_per_node=4 \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=ae_agnostic \
-#     experiment_name=ae_agnostic_drift_vs_mseq99_search \
+#     experiment_name=ae_agnostic_drift_vs_mse_search \
 #     callbacks.anomaly_eff=null \
 #     callbacks.wasserstein_dist=null \
-#     callbacks.stable_mse_mean_top_ckpt=null \
+#     callbacks.stable_mse_q99_ckpt=null \
 #     callbacks.wasserstein_dist_ema_ckpt=null \
-#     ~evaluator.ckpts.single.loss_mse_mean_top_vals \
+#     ~evaluator.ckpts.single.loss_mse_q99 \
 #     ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
 #     evaluator_callbacks.anomaly_efficiency=null \
-#     evaluator_callbacks.mse_operational_mean=null \
+#     evaluator_callbacks.mse_loss_q99=null \
 #     evaluator_callbacks.cap_sn_zb=null \
 #     evaluator_callbacks.wasserstein=null \
 #     evaluator_callbacks.reco=null \
@@ -303,14 +268,49 @@ python3 src/train.py \
 #     optimized_metric_config.main_metric.callback.name=thres_drift \
 #     +optimized_metric_config.main_metric.callback.params.target_rate=0.25 \
 #     optimized_metric_config.main_metric.direction=minimize \
-#     optimized_metric_config.sec_metric.callback.name=mse_q99 \
-#     hydra.sweeper.study_name=drift_vs_mseq99_b16k \
+#     hydra.sweeper.study_name=drift_vs_mse_b16k \
 #     hydra.sweeper.direction='[minimize, minimize]' \
 #     hydra.sweeper.n_trials=100 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+
+# AE agnostic MSE and threshold stability q99.
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=ae_agnostic \
+    experiment_name=ae_agnostic_drift_vs_mseq99_search \
+    callbacks.anomaly_eff=null \
+    callbacks.wasserstein_dist=null \
+    callbacks.stable_mse_mean_top_ckpt=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    ~evaluator.ckpts.single.loss_mse_mean_top_vals \
+    ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    evaluator_callbacks.anomaly_efficiency=null \
+    evaluator_callbacks.mse_operational_mean=null \
+    evaluator_callbacks.cap_sn_zb=null \
+    evaluator_callbacks.wasserstein=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=ae_agnostic_optuna \
+    optimized_metric_config.main_metric.callback.name=thres_drift \
+    +optimized_metric_config.main_metric.callback.params.target_rate=0.25 \
+    optimized_metric_config.main_metric.direction=minimize \
+    optimized_metric_config.sec_metric.callback.name=mse_q99 \
+    hydra.sweeper.study_name=drift_vs_mseq99_b16k \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # Wasserstein Agnostic Searches

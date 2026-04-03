ea0f9924506ad95f4b895ae79e5eb9ceaf2f3746
Patrick <podagiu@ethz.ch>
2026-04-03 23:09:36 +0200

    Prepared VAE cap search.

--- scripts/run_models/runvae.sh
+++ scripts/run_models/runvae.sh
@@ -218,27 +218,39 @@
 # ------------------------
 
 # VAE agnostic hyperparameter search - CAP vs kl.
-# taskset -c 0-2 \
-# python3 src/train.py \
-#     -m \
-#     hydra/launcher=submitit_local \
-#     hydra.launcher.cpus_per_task=1 \
-#     hydra.launcher.gpus_per_node=4 \
-#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-#     experiment=vae_agnostic \
-#     experiment_name=vae_agnostic_cap_vs_kl_search \
-#     callbacks.max_rate_kl_ckpt=null \
-#     callbacks.cvar25_ema_ckpt=null \
-#     evaluator.ckpts.summary=null \
-#     evaluator_callbacks.reco=null \
-#     logger=none \
-#     hparams_search=vae_optuna \
-#     hydra.sweeper.study_name=cap_vs_kl_b16k \
-#     hydra.sweeper.n_trials=100 \
-#     hydra.sweeper.sampler.n_startup_trials=150 \
-#     trainer=gpu \
-#     trainer.max_epochs=50 \
-#     trainer.devices=[0]
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=vae_agnostic \
+    experiment_name=vae_agnostic_cap_vs_kl_search \
+    callbacks.anomaly_eff=null \
+    callbacks.wasserstein_dist=null \
+    callbacks.thres_drift=null \
+    callbacks.stable_kl_q99_ckpt=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    callbacks.thres_drift_ema_ckpt=null \
+    callbacks.thres_drift_q99_ema_ckpt=null \
+    ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+    ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
+    ~evaluator.ckpts.single.loss_kl_raw_q99 \
+    ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    evaluator_callbacks.anomaly_efficiency=null \
+    evaluator_callbacks.kl_loss_q99=null \
+    evaluator_callbacks.thres_drift=null \
+    evaluator_callbacks.wasserstein=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=vae_optuna \
+    hydra.sweeper.study_name=cap_vs_kl_b16k \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # VAE agnostic hyperparameter search - CAP vs kl q99.
@@ -251,9 +263,22 @@
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=vae_agnostic \
 #     experiment_name=vae_agnostic_cap_vs_klq99_search \
-#     callbacks.max_rate_kl_ckpt=null \
-#     callbacks.cvar25_ema_ckpt=null \
-#     evaluator.ckpts.summary=null \
+#     callbacks.anomaly_eff=null \
+#     callbacks.wasserstein_dist=null \
+#     callbacks.thres_drift=null \
+#     callbacks.stable_kl_ckpt=null \
+#     callbacks.wasserstein_dist_ema_ckpt=null \
+#     callbacks.thres_drift_ema_ckpt=null \
+#     callbacks.thres_drift_q99_ema_ckpt=null \
+#     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
+#     ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+#     ~evaluator.ckpts.single.loss_kl_raw_mean_top_vals \
+#     ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+#     evaluator_callbacks.anomaly_efficiency=null \
+#     evaluator_callbacks.kl_operational_mean=null \
+#     evaluator_callbacks.thres_drift=null \
+#     evaluator_callbacks.wasserstein=null \
 #     evaluator_callbacks.reco=null \
 #     logger=none \
 #     hparams_search=vae_optuna \
@@ -354,42 +379,42 @@
 # -----------------------------
 
 # AE agnostic kl and wasserstein distance search.
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=vae_agnostic \
-    experiment_name=vae_agnostic_wasserstein_vs_kl_search \
-    callbacks.anomaly_eff=null \
-    callbacks.thres_drift=null \
-    callbacks.cap_sn_zb=null \
-    callbacks.stable_kl_q99_ckpt=null \
-    callbacks.thres_drift_ema_ckpt=null \
-    callbacks.cap_sn_zb_ema_ckpt=null \
-    callbacks.thres_drift_q99_ema_ckpt=null \
-    ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
-    ~evaluator.ckpts.single.loss_kl_raw_q99 \
-    ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
-    ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-    evaluator_callbacks.anomaly_efficiency=null \
-    evaluator_callbacks.kl_loss_q99=null \
-    evaluator_callbacks.cap_sn_zb=null \
-    evaluator_callbacks.thres_drift=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=vae_optuna \
-    optimized_metric_config.main_metric.callback.name=wasserstein \
-    optimized_metric_config.main_metric.direction=minimize \
-    hydra.sweeper.study_name=wasserstein_vs_kl_b16k \
-    hydra.sweeper.direction='[minimize, minimize]' \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
+# taskset -c 0-2 \
+# python3 src/train.py \
+#     -m \
+#     hydra/launcher=submitit_local \
+#     hydra.launcher.cpus_per_task=1 \
+#     hydra.launcher.gpus_per_node=4 \
+#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+#     experiment=vae_agnostic \
+#     experiment_name=vae_agnostic_wasserstein_vs_kl_search \
+#     callbacks.anomaly_eff=null \
+#     callbacks.thres_drift=null \
+#     callbacks.cap_sn_zb=null \
+#     callbacks.stable_kl_q99_ckpt=null \
+#     callbacks.thres_drift_ema_ckpt=null \
+#     callbacks.cap_sn_zb_ema_ckpt=null \
+#     callbacks.thres_drift_q99_ema_ckpt=null \
+#     ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+#     ~evaluator.ckpts.single.loss_kl_raw_q99 \
+#     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
+#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+#     evaluator_callbacks.anomaly_efficiency=null \
+#     evaluator_callbacks.kl_loss_q99=null \
+#     evaluator_callbacks.cap_sn_zb=null \
+#     evaluator_callbacks.thres_drift=null \
+#     evaluator_callbacks.reco=null \
+#     logger=none \
+#     hparams_search=vae_optuna \
+#     optimized_metric_config.main_metric.callback.name=wasserstein \
+#     optimized_metric_config.main_metric.direction=minimize \
+#     hydra.sweeper.study_name=wasserstein_vs_kl_b16k \
+#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.n_trials=100 \
+#     hydra.sweeper.sampler.n_startup_trials=150 \
+#     trainer=gpu \
+#     trainer.max_epochs=50 \
+#     trainer.devices=[0]
 
 # AE agnostic kl q99 and wasserstein distance search.
 # taskset -c 0-2 \

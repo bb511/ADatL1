8b5520a3e2aab4d08c18c9895b510f2e1d3b8d57
Patrick <podagiu@ethz.ch>
2026-04-03 18:28:14 +0200

    Readied wasserstein VAE search.

--- scripts/run_models/runvae.sh
+++ scripts/run_models/runvae.sh
@@ -357,6 +357,9 @@
 taskset -c 0-2 \
 python3 src/train.py \
     -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
     experiment=vae_agnostic \
     experiment_name=vae_agnostic_wasserstein_vs_kl_search \
@@ -385,11 +388,8 @@ python3 src/train.py \
     hydra.sweeper.n_trials=100 \
     hydra.sweeper.sampler.n_startup_trials=150 \
     trainer=gpu \
-    trainer.max_epochs=1 \
+    trainer.max_epochs=50 \
     trainer.devices=[0]
-    # hydra/launcher=submitit_local \
-    # hydra.launcher.cpus_per_task=1 \
-    # hydra.launcher.gpus_per_node=4 \
 
 # AE agnostic kl q99 and wasserstein distance search.
 # taskset -c 0-2 \

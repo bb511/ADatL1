0106818fed13a0739f2e93a478b2102cc9ea9e45
Patrick <podagiu@ethz.ch>
2026-04-04 13:44:14 +0200

    Readied stability q99 VAE search.

--- scripts/run_models/runvae.sh
+++ scripts/run_models/runvae.sh
@@ -153,34 +153,34 @@
 
 
 # VAE hyperparameter search semi-supervised kl q99.
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=vae \
-    experiment_name=vae_cvar25_vs_klq99_search \
-    callbacks.max_rate_kl_ckpt=null \
-    callbacks.stable_kl_ckpt=null \
-    callbacks.cvar10_ema_ckpt=null \
-    ~evaluator.ckpts.summary.cvar10_ema \
-    ~evaluator.ckpts.single.loss_kl_raw_mean_top_vals \
-    ~evaluator.ckpts.single.eff__ascore_loss_kl_raw_full__brate_0_25kHz \
-    evaluator_callbacks.kl_operational_mean=null \
-    evaluator_callbacks.thres_drift=null \
-    evaluator_callbacks.wasserstein=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=vae_optuna \
-    optimized_metric_config.sec_metric.callback.name=kl_raw_q99 \
-    hydra.sweeper.study_name=cvar25eff_vs_klq99_b16k \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
+# taskset -c 0-2 \
+# python3 src/train.py \
+#     -m \
+#     hydra/launcher=submitit_local \
+#     hydra.launcher.cpus_per_task=1 \
+#     hydra.launcher.gpus_per_node=4 \
+#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+#     experiment=vae \
+#     experiment_name=vae_cvar25_vs_klq99_search \
+#     callbacks.max_rate_kl_ckpt=null \
+#     callbacks.stable_kl_ckpt=null \
+#     callbacks.cvar10_ema_ckpt=null \
+#     ~evaluator.ckpts.summary.cvar10_ema \
+#     ~evaluator.ckpts.single.loss_kl_raw_mean_top_vals \
+#     ~evaluator.ckpts.single.eff__ascore_loss_kl_raw_full__brate_0_25kHz \
+#     evaluator_callbacks.kl_operational_mean=null \
+#     evaluator_callbacks.thres_drift=null \
+#     evaluator_callbacks.wasserstein=null \
+#     evaluator_callbacks.reco=null \
+#     logger=none \
+#     hparams_search=vae_optuna \
+#     optimized_metric_config.sec_metric.callback.name=kl_raw_q99 \
+#     hydra.sweeper.study_name=cvar25eff_vs_klq99_b16k \
+#     hydra.sweeper.n_trials=100 \
+#     hydra.sweeper.sampler.n_startup_trials=150 \
+#     trainer=gpu \
+#     trainer.max_epochs=50 \
+#     trainer.devices=[0]
 
 
 # VAE hyperparameter search semi-supervised cvar 10%.
@@ -335,44 +335,44 @@ python3 src/train.py \
 
 
 # AE agnostic kl and threshold stability.
-# taskset -c 0-2 \
-# python3 src/train.py \
-#     -m \
-#     hydra/launcher=submitit_local \
-#     hydra.launcher.cpus_per_task=1 \
-#     hydra.launcher.gpus_per_node=4 \
-#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-#     experiment=vae_agnostic \
-#     experiment_name=vae_agnostic_drift_vs_klq99_search \
-#     callbacks.anomaly_eff=null \
-#     callbacks.wasserstein_dist=null \
-#     callbacks.cap_sn_zb=null \
-#     callbacks.stable_kl_ckpt=null \
-#     callbacks.wasserstein_dist_ema_ckpt=null \
-#     callbacks.cap_sn_zb_ema_ckpt=null \
-#     callbacks.thres_drift_ema_ckpt=null \
-#     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
-#     ~evaluator.ckpts.single.loss_kl_raw_mean_top_vals \
-#     ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-#     evaluator_callbacks.anomaly_efficiency=null \
-#     evaluator_callbacks.kl_operational_mean=null \
-#     evaluator_callbacks.cap_sn_zb=null \
-#     evaluator_callbacks.wasserstein=null \
-#     evaluator_callbacks.reco=null \
-#     logger=none \
-#     hparams_search=vae_optuna \
-#     optimized_metric_config.main_metric.callback.name=thres_drift \
-#     +optimized_metric_config.main_metric.callback.params.target_rate=286.0 \
-#     optimized_metric_config.main_metric.direction=minimize \
-#     optimized_metric_config.sec_metric.callback.name=kl_raw_q99 \
-#     hydra.sweeper.study_name=drift_vs_klq99_b16k \
-#     hydra.sweeper.direction='[minimize, minimize]' \
-#     hydra.sweeper.n_trials=100 \
-#     hydra.sweeper.sampler.n_startup_trials=150 \
-#     trainer=gpu \
-#     trainer.max_epochs=50 \
-#     trainer.devices=[0]
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=vae_agnostic \
+    experiment_name=vae_agnostic_drift_vs_klq99_search \
+    callbacks.anomaly_eff=null \
+    callbacks.wasserstein_dist=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.stable_kl_ckpt=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    callbacks.thres_drift_ema_ckpt=null \
+    ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
+    ~evaluator.ckpts.single.loss_kl_raw_mean_top_vals \
+    ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    evaluator_callbacks.anomaly_efficiency=null \
+    evaluator_callbacks.kl_operational_mean=null \
+    evaluator_callbacks.cap_sn_zb=null \
+    evaluator_callbacks.wasserstein=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=vae_optuna \
+    optimized_metric_config.main_metric.callback.name=thres_drift \
+    +optimized_metric_config.main_metric.callback.params.target_rate=286.0 \
+    optimized_metric_config.main_metric.direction=minimize \
+    optimized_metric_config.sec_metric.callback.name=kl_raw_q99 \
+    hydra.sweeper.study_name=drift_vs_klq99_b16k \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # Wasserstein Agnostic Searches

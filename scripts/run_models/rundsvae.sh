d20807c94b39a17cefe7bd0ee4a718a7e60fa786
Patrick <podagiu@ethz.ch>
2026-04-07 16:49:00 +0300

    Readied DS VAE wasserstein search.

--- scripts/run_models/rundsvae.sh
+++ scripts/run_models/rundsvae.sh
@@ -324,43 +324,43 @@
 # ---------------------------
 
 # DS VAE agnostic kl and threshold stability.
-taskset -c 18-20 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=dsvae_agnostic \
-    experiment_name=vae_agnostic_drift_vs_kl_search \
-    callbacks.anomaly_eff=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.cap_sn_zb=null \
-    callbacks.stable_kl_q99_ckpt=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    callbacks.cap_sn_zb_ema_ckpt=null \
-    callbacks.thres_drift_q99_ema_ckpt=null \
-    ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
-    ~evaluator.ckpts.single.loss_kl_raw_q99 \
-    ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-    ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-    evaluator_callbacks.anomaly_efficiency=null \
-    evaluator_callbacks.kl_loss_q99=null \
-    evaluator_callbacks.cap_sn_zb=null \
-    evaluator_callbacks.wasserstein=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=dsvae_optuna \
-    optimized_metric_config.main_metric.callback.name=thres_drift \
-    +optimized_metric_config.main_metric.callback.params.target_rate=0.25 \
-    optimized_metric_config.main_metric.direction=minimize \
-    hydra.sweeper.study_name=drift_vs_kl_b16k \
-    hydra.sweeper.direction='[minimize, minimize]' \
-    hydra.sweeper.n_trials=150 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
+# taskset -c 18-20 \
+# python3 src/train.py \
+#     -m \
+#     hydra/launcher=submitit_local \
+#     hydra.launcher.cpus_per_task=1 \
+#     hydra.launcher.gpus_per_node=4 \
+#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+#     experiment=dsvae_agnostic \
+#     experiment_name=vae_agnostic_drift_vs_kl_search \
+#     callbacks.anomaly_eff=null \
+#     callbacks.wasserstein_dist=null \
+#     callbacks.cap_sn_zb=null \
+#     callbacks.stable_kl_q99_ckpt=null \
+#     callbacks.wasserstein_dist_ema_ckpt=null \
+#     callbacks.cap_sn_zb_ema_ckpt=null \
+#     callbacks.thres_drift_q99_ema_ckpt=null \
+#     ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+#     ~evaluator.ckpts.single.loss_kl_raw_q99 \
+#     ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+#     evaluator_callbacks.anomaly_efficiency=null \
+#     evaluator_callbacks.kl_loss_q99=null \
+#     evaluator_callbacks.cap_sn_zb=null \
+#     evaluator_callbacks.wasserstein=null \
+#     evaluator_callbacks.reco=null \
+#     logger=none \
+#     hparams_search=dsvae_optuna \
+#     optimized_metric_config.main_metric.callback.name=thres_drift \
+#     +optimized_metric_config.main_metric.callback.params.target_rate=0.25 \
+#     optimized_metric_config.main_metric.direction=minimize \
+#     hydra.sweeper.study_name=drift_vs_kl_b16k \
+#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.n_trials=150 \
+#     hydra.sweeper.sampler.n_startup_trials=150 \
+#     trainer=gpu \
+#     trainer.max_epochs=50 \
+#     trainer.devices=[0]
 
 
 # DS VAE agnostic kl and threshold stability.
@@ -408,42 +408,42 @@ python3 src/train.py \
 # -----------------------------
 
 # DS VAE agnostic kl and wasserstein distance search.
-# taskset -c 18-20 \
-# python3 src/train.py \
-#     -m \
-#     hydra/launcher=submitit_local \
-#     hydra.launcher.cpus_per_task=1 \
-#     hydra.launcher.gpus_per_node=4 \
-#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-#     experiment=dsvae_agnostic \
-#     experiment_name=vae_agnostic_wasserstein_vs_kl_search \
-#     callbacks.anomaly_eff=null \
-#     callbacks.thres_drift=null \
-#     callbacks.cap_sn_zb=null \
-#     callbacks.stable_kl_q99_ckpt=null \
-#     callbacks.thres_drift_ema_ckpt=null \
-#     callbacks.cap_sn_zb_ema_ckpt=null \
-#     callbacks.thres_drift_q99_ema_ckpt=null \
-#     ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
-#     ~evaluator.ckpts.single.loss_kl_raw_q99 \
-#     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
-#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-#     evaluator_callbacks.anomaly_efficiency=null \
-#     evaluator_callbacks.kl_loss_q99=null \
-#     evaluator_callbacks.cap_sn_zb=null \
-#     evaluator_callbacks.thres_drift=null \
-#     evaluator_callbacks.reco=null \
-#     logger=none \
-#     hparams_search=dsvae_optuna \
-#     optimized_metric_config.main_metric.callback.name=wasserstein \
-#     optimized_metric_config.main_metric.direction=minimize \
-#     hydra.sweeper.study_name=wasserstein_vs_kl_b16k \
-#     hydra.sweeper.direction='[minimize, minimize]' \
-#     hydra.sweeper.n_trials=150 \
-#     hydra.sweeper.sampler.n_startup_trials=150 \
-#     trainer=gpu \
-#     trainer.max_epochs=50 \
-#     trainer.devices=[0]
+taskset -c 18-20 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=dsvae_agnostic \
+    experiment_name=vae_agnostic_wasserstein_vs_kl_search \
+    callbacks.anomaly_eff=null \
+    callbacks.thres_drift=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.stable_kl_q99_ckpt=null \
+    callbacks.thres_drift_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    callbacks.thres_drift_q99_ema_ckpt=null \
+    ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+    ~evaluator.ckpts.single.loss_kl_raw_q99 \
+    ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
+    ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    evaluator_callbacks.anomaly_efficiency=null \
+    evaluator_callbacks.kl_loss_q99=null \
+    evaluator_callbacks.cap_sn_zb=null \
+    evaluator_callbacks.thres_drift=null \
+    evaluator_callbacks.reco=null \
+    logger=none \
+    hparams_search=dsvae_optuna \
+    optimized_metric_config.main_metric.callback.name=wasserstein \
+    optimized_metric_config.main_metric.direction=minimize \
+    hydra.sweeper.study_name=wasserstein_vs_kl_b16k \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=150 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 # DS VAE agnostic kl q99 and wasserstein distance search.
 # taskset -c 18-20 \

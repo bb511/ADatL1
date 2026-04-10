61012e1b646024727377baaeebaaedb2bd4a7fd9
Patrick <podagiu@ethz.ch>
2026-04-10 15:51:34 +0200

    Cleaned up first draft of results.

--- scripts/run_models/rundsvae.sh
+++ scripts/run_models/rundsvae.sh
@@ -5,20 +5,23 @@
 # =======================
 
 # Semi-supervised cvar25 training.
-# taskset -c 6-8 \
+# taskset -c 0-2 \
 # python3 src/train.py \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=dsvae \
-#     run_name=cvar25_t513_high \
-#     trainer.gradient_clip_val=0.0 \
-#     algorithm.encoder.clamp_zlogvar_range='[-20,10]' \
-#     algorithm.optimizer.lr=0.0009593961598492453 \
+#     run_name=cvar25_t372_high \
+#     trainer.gradient_clip_val=2.0 \
+#     algorithm.encoder.clamp_zlogvar_range=[-6,4] \
+#     algorithm.optimizer.lr=0.00011979051144559894 \
 #     algorithm.optimizer.betas='[0.9,0.999]' \
-#     algorithm.optimizer.weight_decay=1e-06 \
-#     algorithm.loss.kl_scale=0.002 \
-#     algorithm.kl_warmup_frac=0.0 \
-#     algorithm.encoder.nodes='[48,24,8]' \
-#     algorithm.encoder.activation=gelu \
+#     algorithm.optimizer.weight_decay=0.0 \
+#     algorithm.loss.kl_scale=0.001 \
+#     algorithm.kl_warmup_frac=0.1 \
+#     algorithm.encoder.activation=relu \
+#     algorithm.encoder.pooling=sum \
+#     algorithm.encoder.add_counts=True \
+#     algorithm.encoder.object_phi_nodes='{FET:[8],egammas:[16,16],jets:[16,16],muons:[8,8],taus:[16,16]}' \
+#     algorithm.encoder.rho_nodes='[16,8]' \
 #     trainer=gpu \
 #     trainer.devices=[0]
 
@@ -44,7 +47,7 @@
 
 
 # CAP training.
-# taskset -c 12-14 \
+# taskset -c 3-5 \
 # python3 src/train.py \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=dsvae_agnostic \
@@ -59,50 +62,56 @@
 #     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
 #     ~evaluator.ckpts.single.loss_kl_raw_q99 \
 #     ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-#     trainer.gradient_clip_val=2.0 \
-#     algorithm.encoder.clamp_zlogvar_range='[-20,10]' \
-#     algorithm.optimizer.lr=7.822713922010138e-05 \
+#     trainer.gradient_clip_val=0.0 \
+#     algorithm.encoder.clamp_zlogvar_range=[-8,6] \
+#     algorithm.optimizer.lr=9.520279159583741e-05 \
 #     algorithm.optimizer.betas='[0.9,0.99]' \
-#     algorithm.optimizer.weight_decay=1e-05 \
+#     algorithm.optimizer.weight_decay=0.001 \
 #     algorithm.loss.kl_scale=0.002 \
-#     algorithm.kl_warmup_frac=0.05 \
-#     algorithm.encoder.nodes='[16,4]' \
+#     algorithm.kl_warmup_frac=0.3 \
 #     algorithm.encoder.activation=relu \
-#     trainer=gpu \
-#     trainer.devices=[2]
-
-
-# Agnostic stability training.
-# taskset -c 18-20 \
-# python3 src/train.py \
-#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-#     experiment=dsvae_agnostic \
-#     run_name=stability_t491_high \
-#     callbacks.wasserstein_dist=null \
-#     callbacks.cap_sn_zb=null \
-#     callbacks.stable_kl_q99_ckpt=null \
-#     callbacks.wasserstein_dist_ema_ckpt=null \
-#     callbacks.cap_sn_zb_ema_ckpt=null \
-#     callbacks.thres_drift_q99_ema_ckpt=null \
-#     ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
-#     ~evaluator.ckpts.single.loss_kl_raw_q99 \
-#     ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-#     trainer.gradient_clip_val=2.0 \
-#     algorithm.encoder.clamp_zlogvar_range='[-10,6]' \
-#     algorithm.optimizer.lr=0.0008423594125278336 \
-#     algorithm.optimizer.betas='[0.9,0.99]' \
-#     algorithm.optimizer.weight_decay=0.0 \
-#     algorithm.loss.kl_scale=0.002 \
-#     algorithm.kl_warmup_frac=0.0 \
-#     algorithm.encoder.nodes='[48,16,8]' \
-#     algorithm.encoder.activation=gelu \
+#     algorithm.encoder.pooling=sum_max \
+#     algorithm.encoder.add_counts=True \
+#     algorithm.encoder.object_phi_nodes='{FET:[8],egammas:[16,16],jets:[16,16],muons:[8,8],taus:[16,16]}' \
+#     algorithm.encoder.rho_nodes='[24,8]' \
 #     trainer=gpu \
 #     trainer.devices=[1]
 
 
+# Agnostic stability training.
+taskset -c 0-2 \
+python3 src/train.py \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=dsvae_agnostic \
+    run_name=stability_t445_high \
+    callbacks.wasserstein_dist=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.stable_kl_q99_ckpt=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    callbacks.thres_drift_q99_ema_ckpt=null \
+    ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+    ~evaluator.ckpts.single.loss_kl_raw_q99 \
+    ~evaluator.ckpts.summary.w1dist_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+    trainer.gradient_clip_val=0.5 \
+    algorithm.encoder.clamp_zlogvar_range=[-20,10] \
+    algorithm.optimizer.lr=5.00019324835396e-05 \
+    algorithm.optimizer.betas='[0.9,0.999]' \
+    algorithm.optimizer.weight_decay=1e-6 \
+    algorithm.loss.kl_scale=0.001 \
+    algorithm.kl_warmup_frac=0.05 \
+    algorithm.encoder.activation=gelu \
+    algorithm.encoder.pooling=sum \
+    algorithm.encoder.add_counts=True \
+    algorithm.encoder.object_phi_nodes='{FET:[8],egammas:[24,16],jets:[24,16],muons:[8,8],taus:[24,16]}' \
+    algorithm.encoder.rho_nodes='[16,8]' \
+    trainer=gpu \
+    trainer.devices=[0]
+
+
 # Agnostic KL-wasserstein training.
-# taskset -c 3-5 \
+# taskset -c 9-11 \
 # python3 src/train.py \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=dsvae_agnostic \
@@ -118,16 +127,19 @@
 #     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
 #     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
 #     trainer.gradient_clip_val=0.5 \
-#     algorithm.encoder.clamp_zlogvar_range='[-6,4]' \
-#     algorithm.optimizer.lr=6.005882209363415e-05 \
+#     algorithm.encoder.clamp_zlogvar_range=[-6,4] \
+#     algorithm.optimizer.lr=5.004971726258936e-05 \
 #     algorithm.optimizer.betas='[0.9,0.99]' \
-#     algorithm.optimizer.weight_decay=0.0 \
+#     algorithm.optimizer.weight_decay=0.001 \
 #     algorithm.loss.kl_scale=0.002 \
-#     algorithm.kl_warmup_frac=0.2 \
-#     algorithm.encoder.nodes='[24,8,4]' \
-#     algorithm.encoder.activation=relu \
+#     algorithm.kl_warmup_frac=0.05 \
+#     algorithm.encoder.activation=gelu \
+#     algorithm.encoder.pooling=mean \
+#     algorithm.encoder.add_counts=False \
+#     algorithm.encoder.object_phi_nodes='{FET:[8],egammas:[16,8],jets:[16,8],muons:[8],taus:[16,8]}' \
+#     algorithm.encoder.rho_nodes='[24,8]' \
 #     trainer=gpu \
-#     trainer.devices=[1]
+#     trainer.devices=[3]
 
 
 # HYPERPARAMETER SEARCHES.
@@ -393,42 +405,42 @@
 # -----------------------------
 
 # DS VAE agnostic kl and wasserstein distance search.
-taskset -c 18-20 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=dsvae_agnostic \
-    experiment_name=dsvae_agnostic_wasserstein_vs_kl_search \
-    callbacks.anomaly_eff=null \
-    callbacks.thres_drift=null \
-    callbacks.cap_sn_zb=null \
-    callbacks.stable_kl_q99_ckpt=null \
-    callbacks.thres_drift_ema_ckpt=null \
-    callbacks.cap_sn_zb_ema_ckpt=null \
-    callbacks.thres_drift_q99_ema_ckpt=null \
-    ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
-    ~evaluator.ckpts.single.loss_kl_raw_q99 \
-    ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
-    ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
-    evaluator_callbacks.anomaly_efficiency=null \
-    evaluator_callbacks.kl_loss_q99=null \
-    evaluator_callbacks.cap_sn_zb=null \
-    evaluator_callbacks.thres_drift=null \
-    evaluator_callbacks.reco=null \
-    logger=none \
-    hparams_search=dsvae_optuna \
-    optimized_metric_config.main_metric.callback.name=wasserstein \
-    optimized_metric_config.main_metric.direction=minimize \
-    hydra.sweeper.study_name=wasserstein_vs_kl_b16k \
-    hydra.sweeper.direction='[minimize, minimize]' \
-    hydra.sweeper.n_trials=150 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
+# taskset -c 18-20 \
+# python3 src/train.py \
+#     -m \
+#     hydra/launcher=submitit_local \
+#     hydra.launcher.cpus_per_task=1 \
+#     hydra.launcher.gpus_per_node=4 \
+#     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+#     experiment=dsvae_agnostic \
+#     experiment_name=dsvae_agnostic_wasserstein_vs_kl_search \
+#     callbacks.anomaly_eff=null \
+#     callbacks.thres_drift=null \
+#     callbacks.cap_sn_zb=null \
+#     callbacks.stable_kl_q99_ckpt=null \
+#     callbacks.thres_drift_ema_ckpt=null \
+#     callbacks.cap_sn_zb_ema_ckpt=null \
+#     callbacks.thres_drift_q99_ema_ckpt=null \
+#     ~evaluator.ckpts.summary.trate286_0kHz_drift_ema \
+#     ~evaluator.ckpts.single.loss_kl_raw_q99 \
+#     ~evaluator.ckpts.summary.trate0_25kHz_drift_ema \
+#     ~evaluator.ckpts.summary.cap_ema_zerobias_vs_SingleNeutrino_E-10-gun \
+#     evaluator_callbacks.anomaly_efficiency=null \
+#     evaluator_callbacks.kl_loss_q99=null \
+#     evaluator_callbacks.cap_sn_zb=null \
+#     evaluator_callbacks.thres_drift=null \
+#     evaluator_callbacks.reco=null \
+#     logger=none \
+#     hparams_search=dsvae_optuna \
+#     optimized_metric_config.main_metric.callback.name=wasserstein \
+#     optimized_metric_config.main_metric.direction=minimize \
+#     hydra.sweeper.study_name=wasserstein_vs_kl_b16k \
+#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.n_trials=150 \
+#     hydra.sweeper.sampler.n_startup_trials=150 \
+#     trainer=gpu \
+#     trainer.max_epochs=50 \
+#     trainer.devices=[0]
 
 # DS VAE agnostic kl q99 and wasserstein distance search.
 # taskset -c 18-20 \

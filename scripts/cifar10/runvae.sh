e2676db9278d2161bddf147ed474cee16aac1cf3
Patrick <podagiu@ethz.ch>
2026-04-17 14:49:15 +0200

    Readied search.

--- scripts/cifar10/runvae.sh
+++ scripts/cifar10/runvae.sh
@@ -173,63 +173,63 @@
 # ------------------------------------------------------------------------
 # CAP search
 # ------------------------------------------------------------------------
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_slurm_clariden \
-    hydra.sweeper.n_jobs=6 \
-    experiment=cifar10/vae_agnostic \
-    experiment_name=cifar10_vae_agnostic_cap_vs_kl_search \
-    callbacks.anomaly_eff=null \
-    callbacks.thres_drift=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.thres_drift_ema_ckpt=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
-    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_reference_normal \
-    evaluation.callbacks.anomaly_efficiency=null \
-    evaluation.callbacks.thres_drift=null \
-    evaluation.callbacks.wasserstein=null \
-    evaluation.callbacks.reco=null \
-    logger=none \
-    hparams_search=imagevae_optuna \
-    hydra.sweeper.study_name=cap_vs_kl \
-    hydra.sweeper.n_trials=600 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-# ------------------------------------------------------------------------
-# Stability search
-# ------------------------------------------------------------------------
 # python3 src/train.py \
 #     -m \
 #     hydra/launcher=submitit_slurm_clariden \
 #     hydra.sweeper.n_jobs=6 \
 #     experiment=cifar10/vae_agnostic \
-#     experiment_name=cifar10_vae_agnostic_drift_vs_kl_search \
+#     experiment_name=cifar10_vae_agnostic_cap_vs_kl_search \
 #     callbacks.anomaly_eff=null \
-#     callbacks.cap_sn_zb=null \
+#     callbacks.thres_drift=null \
 #     callbacks.wasserstein_dist=null \
+#     callbacks.thres_drift_ema_ckpt=null \
 #     callbacks.wasserstein_dist_ema_ckpt=null \
-#     callbacks.cap_sn_zb_ema_ckpt=null \
+#     ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
 #     ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_reference_normal \
-#     ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_reference_normal \
 #     evaluation.callbacks.anomaly_efficiency=null \
-#     evaluation.callbacks.cap_sn_zb=null \
+#     evaluation.callbacks.thres_drift=null \
 #     evaluation.callbacks.wasserstein=null \
 #     evaluation.callbacks.reco=null \
 #     logger=none \
 #     hparams_search=imagevae_optuna \
-#     optimized_metric_config.main_metric.callback.name=thres_drift \
-#     optimized_metric_config.main_metric.direction=minimize \
-#     hydra.sweeper.study_name=drift_vs_kl \
-#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.study_name=cap_vs_kl \
 #     hydra.sweeper.n_trials=600 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+# ------------------------------------------------------------------------
+# Stability search
+# ------------------------------------------------------------------------
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_slurm_clariden \
+    hydra.sweeper.n_jobs=6 \
+    experiment=cifar10/vae_agnostic \
+    experiment_name=cifar10_vae_agnostic_drift_vs_kl_search \
+    callbacks.anomaly_eff=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.wasserstein_dist=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_reference_normal \
+    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_reference_normal \
+    evaluation.callbacks.anomaly_efficiency=null \
+    evaluation.callbacks.cap_sn_zb=null \
+    evaluation.callbacks.wasserstein=null \
+    evaluation.callbacks.reco=null \
+    logger=none \
+    hparams_search=imagevae_optuna \
+    optimized_metric_config.main_metric.callback.name=thres_drift \
+    optimized_metric_config.main_metric.direction=minimize \
+    hydra.sweeper.study_name=drift_vs_kl \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=600 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # ------------------------------------------------------------------------

c68a8853a6a671995381d538ae5e92569181114d
Patrick <podagiu@ethz.ch>
2026-04-17 13:00:24 +0200

    Readied search.

--- scripts/physics/runsvdd.sh
+++ scripts/physics/runsvdd.sh
@@ -196,39 +196,6 @@
 # ------------------------------------------------------------------------
 # CAP training
 # ------------------------------------------------------------------------
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
-    experiment=physics/svdd_agnostic \
-    experiment_name=svdd_agnostic_cap_vs_distq99_search \
-    algorithm.target_rate=0.01 \
-    algorithm.base_rate=null \
-    callbacks.anomaly_eff=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.thres_drift=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    callbacks.thres_drift_ema_ckpt=null \
-    ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
-    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_SingleNeutrino_E-10-gun \
-    evaluation.callbacks.anomaly_efficiency=null \
-    evaluation.callbacks.thres_drift=null \
-    evaluation.callbacks.wasserstein=null \
-    logger=none \
-    hparams_search=svdd_optuna \
-    hydra.sweeper.study_name=cap_vs_distq99 \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-# ------------------------------------------------------------------------
-# Stability training
-# ------------------------------------------------------------------------
 # taskset -c 0-2 \
 # python3 src/train.py \
 #     -m \
@@ -237,30 +204,63 @@ python3 src/train.py \
 #     hydra.launcher.gpus_per_node=4 \
 #     paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
 #     experiment=physics/svdd_agnostic \
-#     experiment_name=svdd_agnostic_drift_vs_distq99_search \
+#     experiment_name=svdd_agnostic_cap_vs_distq99_search \
 #     algorithm.target_rate=0.01 \
 #     algorithm.base_rate=null \
 #     callbacks.anomaly_eff=null \
 #     callbacks.wasserstein_dist=null \
-#     callbacks.cap_sn_zb=null \
+#     callbacks.thres_drift=null \
 #     callbacks.wasserstein_dist_ema_ckpt=null \
-#     callbacks.cap_sn_zb_ema_ckpt=null \
+#     callbacks.thres_drift_ema_ckpt=null \
+#     ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
 #     ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_SingleNeutrino_E-10-gun \
-#     ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_SingleNeutrino_E-10-gun \
 #     evaluation.callbacks.anomaly_efficiency=null \
-#     evaluation.callbacks.cap_sn_zb=null \
+#     evaluation.callbacks.thres_drift=null \
 #     evaluation.callbacks.wasserstein=null \
 #     logger=none \
 #     hparams_search=svdd_optuna \
-#     optimized_metric_config.main_metric.callback.name=thres_drift \
-#     optimized_metric_config.main_metric.direction=minimize \
-#     hydra.sweeper.study_name=drift_vs_distq99 \
-#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.study_name=cap_vs_distq99 \
 #     hydra.sweeper.n_trials=100 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+# ------------------------------------------------------------------------
+# Stability training
+# ------------------------------------------------------------------------
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    paths.raw_data_dir=/data/deodagiu/adl1t_data/parquet_files \
+    experiment=physics/svdd_agnostic \
+    experiment_name=svdd_agnostic_drift_vs_distq99_search \
+    algorithm.target_rate=0.01 \
+    algorithm.base_rate=null \
+    callbacks.anomaly_eff=null \
+    callbacks.wasserstein_dist=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_SingleNeutrino_E-10-gun \
+    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_SingleNeutrino_E-10-gun \
+    evaluation.callbacks.anomaly_efficiency=null \
+    evaluation.callbacks.cap_sn_zb=null \
+    evaluation.callbacks.wasserstein=null \
+    logger=none \
+    hparams_search=svdd_optuna \
+    optimized_metric_config.main_metric.callback.name=thres_drift \
+    optimized_metric_config.main_metric.direction=minimize \
+    hydra.sweeper.study_name=drift_vs_distq99 \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 # ------------------------------------------------------------------------
 # Wasserstein training

a64a8b74ea22131eff4e01250a3f42adab41c1eb
Patrick <podagiu@ethz.ch>
2026-04-28 01:36:56 +0200

    Readied search.

--- scripts/robustad/runvae.sh
+++ scripts/robustad/runvae.sh
@@ -205,66 +205,66 @@
 # ------------------------------------------------------------------------
 # Stability search
 # ------------------------------------------------------------------------
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_slurm_clariden \
-    hydra.sweeper.n_jobs=6 \
-    hydra.sweeper.storage='sqlite:///logs/optuna/robustad/vae.db' \
-    experiment=robustad/vae_agnostic \
-    experiment_name=robustad_vae_agnostic_drift_vs_kl_search \
-    callbacks.anomaly_eff=null \
-    callbacks.cap_sn_zb=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    callbacks.cap_sn_zb_ema_ckpt=null \
-    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
-    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
-    evaluation.callbacks.anomaly_efficiency=null \
-    evaluation.callbacks.cap_sn_zb=null \
-    evaluation.callbacks.wasserstein=null \
-    evaluation.callbacks.reco=null \
-    logger=none \
-    hparams_search=imagevae_optuna \
-    optimized_metric_config.main_metric.callback.name=thres_drift \
-    optimized_metric_config.main_metric.direction=minimize \
-    hydra.sweeper.study_name=drift_vs_kl \
-    hydra.sweeper.direction='[minimize, minimize]' \
-    hydra.sweeper.n_trials=600 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-
-# ------------------------------------------------------------------------
-# Wasserstein search
-# ------------------------------------------------------------------------
 # python3 src/train.py \
 #     -m \
 #     hydra/launcher=submitit_slurm_clariden \
 #     hydra.sweeper.n_jobs=6 \
 #     hydra.sweeper.storage='sqlite:///logs/optuna/robustad/vae.db' \
 #     experiment=robustad/vae_agnostic \
-#     experiment_name=robustad_vae_agnostic_wasserstein_vs_kl_search \
+#     experiment_name=robustad_vae_agnostic_drift_vs_kl_search \
 #     callbacks.anomaly_eff=null \
 #     callbacks.cap_sn_zb=null \
-#     callbacks.thres_drift=null \
-#     callbacks.thres_drift_ema_ckpt=null \
+#     callbacks.wasserstein_dist=null \
+#     callbacks.wasserstein_dist_ema_ckpt=null \
 #     callbacks.cap_sn_zb_ema_ckpt=null \
-#     ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
+#     ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
 #     ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
 #     evaluation.callbacks.anomaly_efficiency=null \
 #     evaluation.callbacks.cap_sn_zb=null \
-#     evaluation.callbacks.thres_drift=null \
+#     evaluation.callbacks.wasserstein=null \
 #     evaluation.callbacks.reco=null \
 #     logger=none \
 #     hparams_search=imagevae_optuna \
-#     optimized_metric_config.main_metric.callback.name=wasserstein \
+#     optimized_metric_config.main_metric.callback.name=thres_drift \
 #     optimized_metric_config.main_metric.direction=minimize \
-#     hydra.sweeper.study_name=wasserstein_vs_kl \
+#     hydra.sweeper.study_name=drift_vs_kl \
 #     hydra.sweeper.direction='[minimize, minimize]' \
 #     hydra.sweeper.n_trials=600 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+
+# ------------------------------------------------------------------------
+# Wasserstein search
+# ------------------------------------------------------------------------
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_slurm_clariden \
+    hydra.sweeper.n_jobs=6 \
+    hydra.sweeper.storage='sqlite:///logs/optuna/robustad/vae.db' \
+    experiment=robustad/vae_agnostic \
+    experiment_name=robustad_vae_agnostic_wasserstein_vs_kl_search \
+    callbacks.anomaly_eff=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.thres_drift=null \
+    callbacks.thres_drift_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
+    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
+    evaluation.callbacks.anomaly_efficiency=null \
+    evaluation.callbacks.cap_sn_zb=null \
+    evaluation.callbacks.thres_drift=null \
+    evaluation.callbacks.reco=null \
+    logger=none \
+    hparams_search=imagevae_optuna \
+    optimized_metric_config.main_metric.callback.name=wasserstein \
+    optimized_metric_config.main_metric.direction=minimize \
+    hydra.sweeper.study_name=wasserstein_vs_kl \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=600 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]

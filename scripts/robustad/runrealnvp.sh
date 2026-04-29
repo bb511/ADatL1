8b78830b6fd6bb1e3cba1a462993b49f80e11107
Patrick <podagiu@ethz.ch>
2026-04-29 02:09:45 +0200

    Readied search.

--- scripts/robustad/runrealnvp.sh
+++ scripts/robustad/runrealnvp.sh
@@ -211,42 +211,6 @@
 # ------------------------------------------------------------------------
 # Stability search
 # ------------------------------------------------------------------------
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.timeout_min=200 \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    hydra.sweeper.storage='sqlite:///logs/optuna/robustad/realnvp.db' \
-    experiment=robustad/realnvp_agnostic \
-    experiment_name=robustad_realnvp_agnostic_drift_vs_logp_search \
-    callbacks.anomaly_eff=null \
-    callbacks.cap_sn_zb=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    callbacks.cap_sn_zb_ema_ckpt=null \
-    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
-    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
-    evaluation.callbacks.anomaly_efficiency=null \
-    evaluation.callbacks.cap_sn_zb=null \
-    evaluation.callbacks.wasserstein=null \
-    logger=none \
-    hparams_search=imagerealnvp_optuna \
-    optimized_metric_config.main_metric.callback.name=thres_drift \
-    optimized_metric_config.main_metric.direction=minimize \
-    hydra.sweeper.study_name=drift_vs_logp \
-    hydra.sweeper.direction='[minimize, minimize]' \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-
-# ------------------------------------------------------------------------
-# Wasserstein search
-# ------------------------------------------------------------------------
 # taskset -c 0-2 \
 # python3 src/train.py \
 #     -m \
@@ -256,25 +220,61 @@ python3 src/train.py \
 #     hydra.launcher.gpus_per_node=4 \
 #     hydra.sweeper.storage='sqlite:///logs/optuna/robustad/realnvp.db' \
 #     experiment=robustad/realnvp_agnostic \
-#     experiment_name=robustad_realnvp_agnostic_wasserstein_vs_logp_search \
+#     experiment_name=robustad_realnvp_agnostic_drift_vs_logp_search \
 #     callbacks.anomaly_eff=null \
 #     callbacks.cap_sn_zb=null \
-#     callbacks.thres_drift=null \
-#     callbacks.thres_drift_ema_ckpt=null \
+#     callbacks.wasserstein_dist=null \
+#     callbacks.wasserstein_dist_ema_ckpt=null \
 #     callbacks.cap_sn_zb_ema_ckpt=null \
-#     ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
+#     ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
 #     ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
 #     evaluation.callbacks.anomaly_efficiency=null \
 #     evaluation.callbacks.cap_sn_zb=null \
-#     evaluation.callbacks.thres_drift=null \
+#     evaluation.callbacks.wasserstein=null \
 #     logger=none \
 #     hparams_search=imagerealnvp_optuna \
-#     optimized_metric_config.main_metric.callback.name=wasserstein \
+#     optimized_metric_config.main_metric.callback.name=thres_drift \
 #     optimized_metric_config.main_metric.direction=minimize \
-#     hydra.sweeper.study_name=wasserstein_vs_logp \
+#     hydra.sweeper.study_name=drift_vs_logp \
 #     hydra.sweeper.direction='[minimize, minimize]' \
 #     hydra.sweeper.n_trials=100 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+
+# ------------------------------------------------------------------------
+# Wasserstein search
+# ------------------------------------------------------------------------
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.timeout_min=200 \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    hydra.sweeper.storage='sqlite:///logs/optuna/robustad/realnvp.db' \
+    experiment=robustad/realnvp_agnostic \
+    experiment_name=robustad_realnvp_agnostic_wasserstein_vs_logp_search \
+    callbacks.anomaly_eff=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.thres_drift=null \
+    callbacks.thres_drift_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
+    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
+    evaluation.callbacks.anomaly_efficiency=null \
+    evaluation.callbacks.cap_sn_zb=null \
+    evaluation.callbacks.thres_drift=null \
+    logger=none \
+    hparams_search=imagerealnvp_optuna \
+    optimized_metric_config.main_metric.callback.name=wasserstein \
+    optimized_metric_config.main_metric.direction=minimize \
+    hydra.sweeper.study_name=wasserstein_vs_logp \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]

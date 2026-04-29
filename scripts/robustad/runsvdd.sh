a34a3a4f2511ad1411727d31a6eea1b7332f4469
Patrick <podagiu@ethz.ch>
2026-04-29 02:19:17 +0200

    Readied search.

--- scripts/robustad/runsvdd.sh
+++ scripts/robustad/runsvdd.sh
@@ -179,38 +179,6 @@
 # ------------------------------------------------------------------------
 # CAP search
 # ------------------------------------------------------------------------
-taskset -c 0-2 \
-python3 src/train.py \
-    -m \
-    hydra/launcher=submitit_local \
-    hydra.launcher.timeout_min=200 \
-    hydra.launcher.cpus_per_task=1 \
-    hydra.launcher.gpus_per_node=4 \
-    hydra.sweeper.storage='sqlite:///logs/optuna/robustad/svdd.db' \
-    experiment=robustad/svdd_agnostic \
-    experiment_name=robustad_svdd_agnostic_cap_vs_dist_search \
-    callbacks.anomaly_eff=null \
-    callbacks.thres_drift=null \
-    callbacks.wasserstein_dist=null \
-    callbacks.thres_drift_ema_ckpt=null \
-    callbacks.wasserstein_dist_ema_ckpt=null \
-    ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
-    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
-    evaluation.callbacks.anomaly_efficiency=null \
-    evaluation.callbacks.thres_drift=null \
-    evaluation.callbacks.wasserstein=null \
-    logger=none \
-    hparams_search=imagesvdd_optuna \
-    hydra.sweeper.study_name=cap_vs_dist \
-    hydra.sweeper.n_trials=100 \
-    hydra.sweeper.sampler.n_startup_trials=150 \
-    trainer=gpu \
-    trainer.max_epochs=50 \
-    trainer.devices=[0]
-
-# ------------------------------------------------------------------------
-# Stability search
-# ------------------------------------------------------------------------
 # taskset -c 0-2 \
 # python3 src/train.py \
 #     -m \
@@ -220,28 +188,60 @@ python3 src/train.py \
 #     hydra.launcher.gpus_per_node=4 \
 #     hydra.sweeper.storage='sqlite:///logs/optuna/robustad/svdd.db' \
 #     experiment=robustad/svdd_agnostic \
-#     experiment_name=robustad_svdd_agnostic_drift_vs_dist_search \
+#     experiment_name=robustad_svdd_agnostic_cap_vs_dist_search \
 #     callbacks.anomaly_eff=null \
-#     callbacks.cap_sn_zb=null \
+#     callbacks.thres_drift=null \
 #     callbacks.wasserstein_dist=null \
+#     callbacks.thres_drift_ema_ckpt=null \
 #     callbacks.wasserstein_dist_ema_ckpt=null \
-#     callbacks.cap_sn_zb_ema_ckpt=null \
+#     ~evaluation.evaluator.ckpts.summary.operational_drift_ema \
 #     ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
-#     ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
 #     evaluation.callbacks.anomaly_efficiency=null \
-#     evaluation.callbacks.cap_sn_zb=null \
+#     evaluation.callbacks.thres_drift=null \
 #     evaluation.callbacks.wasserstein=null \
 #     logger=none \
 #     hparams_search=imagesvdd_optuna \
-#     optimized_metric_config.main_metric.callback.name=thres_drift \
-#     optimized_metric_config.main_metric.direction=minimize \
-#     hydra.sweeper.study_name=drift_vs_dist \
-#     hydra.sweeper.direction='[minimize, minimize]' \
+#     hydra.sweeper.study_name=cap_vs_dist \
 #     hydra.sweeper.n_trials=100 \
 #     hydra.sweeper.sampler.n_startup_trials=150 \
 #     trainer=gpu \
 #     trainer.max_epochs=50 \
 #     trainer.devices=[0]
+
+# ------------------------------------------------------------------------
+# Stability search
+# ------------------------------------------------------------------------
+taskset -c 0-2 \
+python3 src/train.py \
+    -m \
+    hydra/launcher=submitit_local \
+    hydra.launcher.timeout_min=200 \
+    hydra.launcher.cpus_per_task=1 \
+    hydra.launcher.gpus_per_node=4 \
+    hydra.sweeper.storage='sqlite:///logs/optuna/robustad/svdd.db' \
+    experiment=robustad/svdd_agnostic \
+    experiment_name=robustad_svdd_agnostic_drift_vs_dist_search \
+    callbacks.anomaly_eff=null \
+    callbacks.cap_sn_zb=null \
+    callbacks.wasserstein_dist=null \
+    callbacks.wasserstein_dist_ema_ckpt=null \
+    callbacks.cap_sn_zb_ema_ckpt=null \
+    ~evaluation.evaluator.ckpts.summary.w1dist_ema_normal_vs_shifted_normal_all \
+    ~evaluation.evaluator.ckpts.summary.cap_ema_normal_vs_shifted_normal_all \
+    evaluation.callbacks.anomaly_efficiency=null \
+    evaluation.callbacks.cap_sn_zb=null \
+    evaluation.callbacks.wasserstein=null \
+    logger=none \
+    hparams_search=imagesvdd_optuna \
+    optimized_metric_config.main_metric.callback.name=thres_drift \
+    optimized_metric_config.main_metric.direction=minimize \
+    hydra.sweeper.study_name=drift_vs_dist \
+    hydra.sweeper.direction='[minimize, minimize]' \
+    hydra.sweeper.n_trials=100 \
+    hydra.sweeper.sampler.n_startup_trials=150 \
+    trainer=gpu \
+    trainer.max_epochs=50 \
+    trainer.devices=[0]
 
 
 # ------------------------------------------------------------------------

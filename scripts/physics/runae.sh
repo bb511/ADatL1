588d41cb2ec493d3c39e461ee8f37697b54dce69
leon-behrens <behrens.leon@gmail.com>
2026-05-22 15:40:04 +0200

    change data directory and epoch=100

--- scripts/physics/runae.sh
+++ scripts/physics/runae.sh
@@ -11,7 +11,7 @@
 # ------------------------------------------------------------------------
 # taskset -c 0-2 \
 python3 src/train.py \
-    paths.raw_data_dir=/path/to/adl1t_data/parquet_files \
+    paths.raw_data_dir=../../03_Data/adl1t_data/parquet_files \
     experiment=physics/ae \
     run_name=cvar25_t169 \
     algorithm.optimizer.lr=0.0019859329798336714 \
@@ -21,7 +21,7 @@ python3 src/train.py \
     algorithm.optimizer.weight_decay=1e-06 \
     algorithm.encoder.nodes='[64,32,8]' \
     algorithm.input_noise_std=0.0 \
-    trainer.max_epochs=2 \
+    trainer.max_epochs=100 \
     trainer=gpu \
     trainer.devices=[0]
 

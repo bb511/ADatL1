36ab825baa9ce99712ba00555d1a8a99f7a43c79
leon-behrens <behrens.leon@gmail.com>
2026-05-25 10:33:02 +0200

    Add script to run MI Aware AE based on AE

---
+++ scripts/physics/runmiawareae.sh
@@ -0,0 +1,19 @@
+python3 src/train.py \
+    paths.raw_data_dir=../../03_Data/adl1t_data/parquet_files \
+    experiment=physics/mi_aware_ae \
+    run_name="MI_Aware_AE_Run_1_Gamma_1e-06" \
+    algorithm.optimizer.lr=0.0019859329798336714 \
+    algorithm.delta=10.0 \
+    trainer.gradient_clip_val=5.0 \
+    algorithm.optimizer.betas='[0.9,0.999]' \
+    algorithm.optimizer.weight_decay=1e-06 \
+    algorithm.encoder.nodes='[64,32,8]' \
+    algorithm.input_noise_std=0.0 \
+    algorithm.use_bernoulli_bottleneck=true \
+    algorithm.gamma=1e-06 \
+    algorithm.bottleneck_temperature=1.0 \
+    algorithm.deterministic_eval=true \
+    trainer.max_epochs=100 \
+    trainer=gpu \
+    trainer.devices='[0]'
+

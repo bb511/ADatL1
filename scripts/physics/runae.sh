c44d94514613596781be61193d7f975b9bddace0
leon-behrens <behrens.leon@gmail.com>
2026-06-03 09:46:47 +0200

    feat: adjust existing sh file to include bernoulli mi runs with gamma

--- scripts/physics/runae.sh
+++ scripts/physics/runae.sh
@@ -10,22 +10,24 @@
 # Semi-supervised cvar25 training
 # ------------------------------------------------------------------------
 # taskset -c 0-2 \
+
 python3 src/train.py \
     paths.raw_data_dir=../../03_Data/adl1t_data/parquet_files \
     experiment=physics/ae \
-    run_name="Bernoulli_MI_AE_Gamma_0.1_Run_1" \
+    run_name="Bernoulli_MI_AE_Gamma_0.2_Run_1" \
+    logger=mlflow \
     algorithm.optimizer.lr=0.0019859329798336714 \
     algorithm.delta=10.0 \
+    algorithm.mi_gamma=0.2 \
+    algorithm.mi_temperature=6.0 \
     trainer.gradient_clip_val=5.0 \
     algorithm.optimizer.betas='[0.9,0.999]' \
     algorithm.optimizer.weight_decay=1e-06 \
     algorithm.encoder.nodes='[64,32,8]' \
     algorithm.input_noise_std=0.0 \
-    algorithm.gamma=0.1 \
-    algorithm.reduction=sum \
-    trainer.max_epochs=50 \
+    trainer.max_epochs=30 \
     trainer=gpu \
-    trainer.devices=[0]
+    trainer.devices='[0]'
 
 # ------------------------------------------------------------------------
 # Semi-supervised cvar10 training

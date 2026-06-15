747b86278c8474aeacd8d791c85bd42060273bcc
leon-behrens <behrens.leon@gmail.com>
2026-06-15 22:13:11 +0200

    fix: change configs

--- scripts/physics/runae-gamma0.sh
+++ scripts/physics/runae-gamma0.sh
@@ -1,17 +1,17 @@
 python3 src/train.py \
     paths.raw_data_dir=../../03_Data/adl1t_data/parquet_files \
     experiment=physics/ae \
-    run_name="Bernoulli_MI_AE_Gamma_10.0_Run_1" \
+    run_name="Bernoulli_MI_AE_Ep_10_Gamma_0.0_Run_1" \
     logger=mlflow \
     algorithm.optimizer.lr=0.0019859329798336714 \
-    algorithm.delta=0.0 \
-    algorithm.mi_gamma=10.0 \
+    algorithm.delta=1.0 \
+    algorithm.mi_gamma=0.0 \
     algorithm.mi_temperature=6.0 \
     trainer.gradient_clip_val=5.0 \
     algorithm.optimizer.betas='[0.9,0.999]' \
     algorithm.optimizer.weight_decay=1e-06 \
     algorithm.encoder.nodes='[64,32,8]' \
     algorithm.input_noise_std=0.0 \
-    trainer.max_epochs=30 \
+    trainer.max_epochs=10 \
     trainer=gpu \
     trainer.devices='[0]'
